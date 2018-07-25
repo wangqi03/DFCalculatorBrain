6227354ba8efcb4b89393f28eb153cc01548ec8d
wanghaojiao <wanghaojiao@cmhi.chinamobile.com>
2018-07-25 15:03:55 +0800

    created a demo project

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by wanghaojiao on 2018/7/25.
+//  Copyright © 2018年 wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by wanghaojiao on 2018/7/25.
+//  Copyright © 2018年 wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
